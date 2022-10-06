846212c7b9b5034f8cec0139d733296438dbcaf1
Umang balicha <54236975+umang331@users.noreply.github.com>
2022-10-06 15:40:52 +0530

    Add files via upload

---
+++ project-209/client.py
@@ -0,0 +1,107 @@
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import Tk
+import os
+from playsound import playsound
+import pygame
+from pygame import mixer
+
+PORT = 8050
+IP_ADDRESS = '127.0.0.1'
+SERVER = None
+BUFFER_SIZE = 4096
+name = None
+listbox = None
+filePathLabel = None
+
+global song_counter
+song_counter = 0
+
+
+def play():
+    global song_selected
+    song_selected = listbox.get(ANCHOR)
+
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.play()
+    if(song_selected != ""):
+        infoLabel.configure(text="Now Playing: " + song_selected)
+    else:
+        infoLabel.configure(text="")
+
+
+def stop():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.pause()
+    infoLabel.configure(text="")
+
+
+def musicWindow():
+    global song_counter
+    global filePathLabel
+    global listbox
+    global infoLabel
+
+    window = Tk()
+    window.title('Music Window')
+    window.geometry("300x350")
+    window.configure(bg='LightSkyBlue')
+
+    selectlabel = Label(window, text="Select Song",
+                        bg='LightSkyBlue', font=("Calibri", 8))
+    selectlabel.place(x=2, y=1)
+
+    listbox = Listbox(window, height=10, width=39, activestyle='dotbox',
+                      bg='LightSkyBlue', borderwidth=2, font=("Calibri", 10))
+    listbox.place(x=10, y=18)
+    for file in os.listdir('shared_files'):
+        filename = os.fsdecode(file)
+        listbox.insert(song_counter, filename)
+        song_counter = song_counter + 1
+
+    scrollbar1 = Scrollbar(listbox)
+    scrollbar1.place(relheight=1, relx=1)
+    scrollbar1.config(command=listbox.yview)
+
+    playButton = Button(window, text="Play", width=10, bd=1,
+                        bg='SkyBlue', font=("Calibri", 10), command=play)
+    playButton.place(x=30, y=200)
+
+    stop = Button(window, text="Stop", bd=1, width=10,
+                  bg='SkyBlue', font=("Calibri", 10), command=stop)
+    stop.place(x=200, y=200)
+
+    upload = Button(window, text="Upload", width=10, bd=1,
+                    bg='SkyBlue', font=("Calibri", 10))
+    upload.place(x=30, y=300)
+
+    download = Button(window, text="Download", width=10,
+                      bd=1, bg='SkyBlue', font=("Calibri", 10))
+    download.place(x=200, y=300)
+
+    infoLabel = Label(window, text="", fg="blue",
+                      bg='SkyBlue', font=("Calibri", 8))
+    infoLabel.place(x=4, y=330)
+
+    window.mainloop()
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+    global song_counter
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+
+    musicWindow()
+
+
+setup()
